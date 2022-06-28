958009603f73470e85a61d12b33cab1291580771
nekita69 <kne.21@uni-dubna.ru>
2022-06-28 18:17:56 +0300

    Add func delete_file() in main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # Командный проект от группы "Трио" с учатниками: Батаев Илья, Никита Козлов, Хомяков Кирилл
 # Капитан - Батаев И.С.
 
+#nikita - удаление
+def delete_file():
+    file_path = 'file.txt'
+    try:
+        os.remove(file_path)
+        return True
+    except:
+        print("The system cannot find the file specified")
+        return False
+        
