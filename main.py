94b87028d48ac9b16fd34449dffb529f79664a22
Bis <108336792+Bis21@users.noreply.github.com>
2022-06-29 10:38:14 +0300

    Added fuction  to write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#User2(Kirill): чтение
-def read_txt_file():
-    file = 'file.txt'
-    with open(file,'r', encoding='utf-8') as f:
-        data = f.read()
-        print(data)
-        return data
+# Командный проект от группы "Трио" с учатниками: Батаев Илья, Никита Козлов, Хомяков Кирилл
+# Капитан - Батаев И.С.
+def write_in_txt_file():
+	with open('test.txt','w') as writer:
+		writer.write('I love this GIT')
+		return True
