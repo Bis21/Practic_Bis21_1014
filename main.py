cd9bd241d58053b8a418d834b98db9213f384ad0
Bis <108336792+Bis21@users.noreply.github.com>
2022-06-29 11:17:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,11 @@ def write_in_txt_file():
 	with open('test.txt','w') as writer:
 		writer.write('I love this GIT')
 		return True
+
+#User2(Kirill): чтение
+def read_txt_file():
+    file = 'file.txt'
+    with open(file,'r', encoding='utf-8') as f:
+        data = f.read()
+        print(data)
+        return data
