ad74dd182d310d647b3b4362407dcf63b25374c6
KilirrKH <manykirich@yandex.ru>
2022-07-07 01:53:42 +0500

    Test for report

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#User2(Kirill): чтение
-def read_txt_file():
+
+
+def test():
     file = 'file.txt'
-    with open(file,'r', encoding='utf-8') as f:
-        data = f.read()
+    with open(file, 'r', encoding='utf-8') as f:
+        data=f.read()
+        print("Test")
         print(data)
         return data
